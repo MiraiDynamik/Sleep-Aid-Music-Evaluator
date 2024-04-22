082e177d6d14707b600545352e16df5177519574
BadStructure <84621744+BadStructure@users.noreply.github.com>
2024-04-22 15:56:04 +0800

    PERFORMANCE ↑ ↑ ↑

--- web/js/script.js
+++ web/js/script.js
@@ -1,47 +1,43 @@
-document.getElementById("btn-selectFolder").addEventListener("click", click);
+// Array for music-score pairs
+var existingItems=new Array();
+
 
 //Handle click
+document.getElementById("btn-selectFolder").addEventListener("click", click);
 function click() {
     eel.select_folder()(function(eel_select_return) {
-        let selected_files = eel_select_return[0];
-        let root = eel_select_return[1];
+        let selectedFiles = eel_select_return[0];
+        let selectedRoot = eel_select_return[1];
 
-        //Display a spinner
+         // Display a spinner
         var spinner_place = document.getElementById("spinnerPlace");
         spinner_place.innerHTML = '<div class="spinner-border text-light mt-2" role="status"></div>';
 
+        // Prevent repeating items
+        selectedFiles = selectedFiles.filter(file => !existingItems.some(item => item[0] === file));
+
         // Process selected files with python
-        eel.process_selected_files(selected_files, root)(function(eel_process_return) {
+        eel.process_selected_files(selectedFiles, selectedRoot)(function(eel_process_return) {
             console.log(eel_process_return);
-            display(eel_process_return);
-            //Remove the spinner
+            display_results(eel_process_return);
+
+            // Remove the spinner
             spinner_place.innerHTML = '<p class="word text-center m-0">that contains any .mp3 files</p>';
         });
     });
 }
 
-//Array for music-score pairs, always exists
+// Array for music-score pairs, always exists
 var existingItems=new Array();
 
-//Prevent repetition，sort when add item
-function display(results) {
+// Prevent repetition，sort when add item
+function display_results(results) {
     for (var i = 0; i < results.length; i++) {
-        let result = results[i];
-        let exists = false;
-        // Check repeating items, existingItems[0] and result[0] are the names of the files
-        for (var j = 0; j < existingItems.length; j++) {
-            if (existingItems[j][0] == result[0]) {
-                exists = true;
-                break;
-            }
-        }
-        if (!exists) {
-            var place = find_place(result[1]); // results[1] is the score of the given music
-            existingItems.splice(place, 0, result); // Sort results by score
-        }
+        var place = find_insertion_index(results[i][1]); // results[i][1] is the score of music file i
+        existingItems.splice(place, 0, results[i]); // Sort results by score
     }
 
-    //Display all results
+    // Display all results
     var out_list = document.getElementById("outList");
     out_list.innerHTML = '';
     existingItems.forEach(function(item) {
@@ -52,7 +48,8 @@ function display(results) {
     });
 }
 
-function find_place(scoreInt) {
+
+function find_insertion_index(scoreInt) {
     for (var i = 0; i < existingItems.length; i++) {
         if (scoreInt >= existingItems[i][1]) {return i}
     }
